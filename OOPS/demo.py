5cbf092d8e188f05e3692c1159c5705707941d16
selvaganeshv778 <116898028+selvaganeshv778@users.noreply.github.com>
2023-03-18 22:48:07 +0530

    commiting from terminal

---
+++ OOPS/demo.py
@@ -0,0 +1,16 @@
+user = {
+    "name":"selva",
+    "age":25,
+    "moblieNumber":9789048887,
+    "name":"Ganesh",
+    "otherInfo":{
+        "work":"software",
+        "qualification":"BE"
+    }
+}
+
+print(user)
+
+print(user.values())
+
+print(user['otherInfo']['qualification'])
